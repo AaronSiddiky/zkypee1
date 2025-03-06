0d7e1c8d287bdb5410bb11d8475ef5efe5fcae61
LeonardHolter <leonard.holter@columbia.edu>
2025-03-06 17:42:51 -0500

    dfsfddf

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Basic favicon with explicit sizes */}
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+
+        {/* PNG favicons */}
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Web Manifest */}
+        <link rel="manifest" href="/site.webmanifest" />
+
         <style
           dangerouslySetInnerHTML={{
             __html: `
