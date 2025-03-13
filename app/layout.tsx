23da3302b14a58ee1aad65837513228e17b28866
LeonardHolter <leonard.holter@columbia.edu>
2025-03-13 19:19:59 -0400

    skkk

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16917828312"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16917828312');
+            `,
+          }}
+        />
+
         {/* Basic favicon with explicit sizes */}
         <link rel="icon" href="/favicon.ico" sizes="48x48" />
 
